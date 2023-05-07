278772d8eb3391f90fb70dd1efa9671b5e306181
Hoang Le Chau <44614317+hoanglechau@users.noreply.github.com>
2023-05-07 19:58:29 +0700

    Create async.ts

---
+++ src/middleware/async.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+const asyncWrapper = (
+  fn: (req: Request, res: Response, next: NextFunction) => void
+) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default asyncWrapper;
